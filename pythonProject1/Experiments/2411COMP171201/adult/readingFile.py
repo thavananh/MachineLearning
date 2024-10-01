9594ab74d1ba142209345ea10db41b8870552b36
duycute <thavananh@gmail.com>
2024-10-02 02:54:51 +0700

    123123
    Signed-off-by: duycute <thavananh@gmail.com>

---
+++ pythonProject1/Experiments/2411COMP171201/adult/readingFile.py
@@ -0,0 +1,21 @@
+import csv
+f = open("adult.data", "r")
+i = 0
+# data_list = ['age', 'work class', 'fnlwgt', 'education', 'education-num', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'income']
+data_list = []
+for item in f:
+    tmp_list = []
+    for item1 in item.split(","):
+        tmp_list.append(item1)
+    data_list.append(tmp_list)
+
+headers = ['age', 'work class', 'education', 'education-num', 'marital-status', 'occupation',
+               'relationship', 'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'income']
+
+with open('adult.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(headers)
+    for row in data_list:
+        # print(row)
+        print([item.strip() for item in row])
+        writer.writerow([item.strip() for item in row])
